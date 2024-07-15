87fd603a3f3c7466cb1e3808102441823c01f082
w1ll1ann <willearth255@gmail.com>
2024-07-15 16:38:03 -0300

    Definicao da pagina 'CadastrarNovaAtividade'

---
+++ StudyMember/ClientApp/src/app/CadastrarNovaAtividade/CadastrarNovaAtividade.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastrar-nova-atividade',
+  templateUrl: './CadastrarNovaAtividade.component.html',
+  styleUrls: ['./CadastrarNovaAtividade.component.css']
+})
+export class CadastrarNovaAtividadeComponent {
+  registerExerciseForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerExerciseForm = this.fb.group({
+      nomeAtividade: ['', Validators.required],
+      dataPrazoAtividade: ['', Validators.required],
+      tipoAtividade: ['', Validators.required],
+      valorAtividade: ['']
+    });
+  }
+
+  onSubmit() {
+    if (this.registerExerciseForm.valid) {
+      const formData = this.registerExerciseForm.value;
+      console.log('Formulário enviado!', formData);
+    } else {
+      console.log('Formulário inválido');
+    }
+  }
+}
